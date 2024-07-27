2f0eb19e0a3d230a2d0146fa9b3e1fb06e8f3b5e
snOOpyCodesss <snoopy140224@gmail.com>
2024-07-27 15:58:48 +0530

    added login functionality

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const { loading, login } = useLogin();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(username, password);
+	};
+
+
   return (
     <div className=' flex flex-col w-full h-screen justify-center items-center bg-zinc-900'>
       <div className=' border-[1px] rounded-lg border-blue-300 flex flex-col items-center justify-center min-w-96 h-88 mx-auto'>
@@ -15,14 +29,17 @@ const Login = () => {
           </h1>
         </div>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className=' text-blue-300'>
               <label  className='label p-2'>
                 <span className='text-blue-300 text-base label-text'> 
                   Username 
                 </span>
               </label>
-            <input type="text" placeholder="Enter your alias" className="input input-bordered input-info w-full max-w-xs" />    
+            <input type="text" placeholder="Enter your alias" className="input input-bordered input-info w-full max-w-xs"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />    
             </div>
 
             <div>
@@ -31,24 +48,21 @@ const Login = () => {
                     Password
                   </span>
                 </label>
-                <input type="password" placeholder="shushhh it's a secret key!" className="input input-bordered input-info w-full h-10" />    
+                <input type="password" placeholder="shushhh it's a secret key!" className="input input-bordered input-info w-full h-10"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}                
+                />    
             </div>
-
-
 
  					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-3 inline-block'>
  						{"Don't"} have an account?
- 					</Link>
- 					<div>
- 						<button className='btn btn-block btn-sm mt-2'>Login</button>
- 					</div>
-           
-					{/* <div>
+ 					</Link>     
+
+					<div>
 						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
 							{loading ? <span className='loading loading-spinner '></span> : "Login"}
 						</button>
-					</div> */}
-
+					</div>
 
           </form>
         </div>
