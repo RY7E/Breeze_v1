2961c1f38442f33b3962465f886bccfb8cb82376
Aditya Basu <adityabasu2017@gmail.com>
2024-08-03 13:24:40 +0530

    deployment attempt #2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), resolve({
+		extensions: ['.js', '.jsx'],
+	  }),],
 	server: {
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://breeze-eight.vercel.app/",
 			},
 		},
 	},
