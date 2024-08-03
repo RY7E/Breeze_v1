64be143b1e70d4d261c3339a9f3fd664dd1be7a3
Aditya Basu <adityabasu2017@gmail.com>
2024-08-03 13:26:10 +0530

    deployment attempt #3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), resolve({
-		extensions: ['.js', '.jsx'],
-	  }),],
+	plugins: [react()],
 	server: {
 		port: 3000,
 		proxy: {
